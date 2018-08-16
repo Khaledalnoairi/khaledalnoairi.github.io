e6d19c72ccb7fd61854dc15164b9364c3d5b7a1b
Khaledalnoairi <42443272+Khaledalnoairi@users.noreply.github.com>
2018-08-16 18:20:23 +0300

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    print("start")
+    i=0
+    while(i<100):
+        print("khaled {}".format(i))
+        i+=2 #2 means print even number only
+
+    print("FINISH")
+
+if __name__ == '__main__':main()
